ceaaa291edc840ef367dee1a7284a35b3bd5d5d4
dl <977066045@qq.com>
2020-01-04 19:10:05 +0800

    dl提交

--- js/dlindex.js
+++ js/dlindex.js
@@ -28,6 +28,34 @@ window.addEventListener('load',function(){
     }
     // 动画封装函数结束
 
+    // 返回顶部动画封装函数
+    function animateTop(obj, target, callback) {
+        // console.log(callback);  callback = function() {}  调用的时候 callback()
+
+        // 先清除以前的定时器，只保留当前的一个定时器执行
+        clearInterval(obj.timer);
+        obj.timer = setInterval(function() {
+            // 步长值写到定时器的里面
+            // 把我们步长值改为整数 不要出现小数的问题
+            // var step = Math.ceil((target - obj.offsetLeft) / 10);
+            var step = (target - window.pageYOffset) / 10;
+            step = step > 0 ? Math.ceil(step) : Math.floor(step);
+            if (window.pageYOffset == target) {
+                // 停止动画 本质是停止定时器
+                clearInterval(obj.timer);
+                // 回调函数写到定时器结束里面
+                // if (callback) {
+                //     // 调用函数
+                //     callback();
+                // }
+                callback && callback();
+            }
+            // 把每次加1 这个步长值改为一个慢慢变小的值  步长公式：(目标值 - 现在的位置) / 10
+            // obj.style.left = window.pageYOffset + step + 'px';
+            window.scroll(0, window.pageYOffset + step);
+        }, 15);
+    }
+
     // banner轮播图开始
     var bannerWidt = document.querySelector('.bannerWidt');
     var bannerWidth = bannerWidt.offsetWidth;
@@ -128,6 +156,27 @@ window.addEventListener('load',function(){
     advices.addEventListener('mouseleave',function(){
         animate(advices,bannerWidth-warmImgWidth);
     });
+
+    // 返回顶部按钮
+    var backTop = document.querySelector('.back_top_img');
+    var backHeight = document.querySelector('.content_new');
+    // var contentTop = document.querySelector('.content_nav');
+    document.addEventListener('scroll',function(){
+        // console.log(window.pageYOffset);
+        if(window.pageYOffset > banner.offsetTop && window.pageYOffset <= backHeight.offsetTop){  
+            backTop.classList.add('back_top_img','fixed');
+        } else { 
+            console.log(111);
+            backTop.classList.remove('fixed');
+        }
+    });
+    backTop.addEventListener('click',function(){
+         // 里面的x和y 不跟单位的 直接写数字即可
+            // window.scroll(0, 0);
+            // 因为是窗口滚动 所以对象是window
+            animateTop(window, 0);
+    })
+    
 
 
     // 运单查询开始
