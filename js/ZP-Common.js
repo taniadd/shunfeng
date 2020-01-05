0c1816c753edf94b7a72f4b7ad79094dce3badef
zp <2425587616@qq.com>
2020-01-05 21:26:15 +0800

    周鹏一月五号

--- js/ZP-Common.js
+++ js/ZP-Common.js
@@ -3,6 +3,7 @@ $(() => {
         var banner = document.querySelector('.banner')
         var banTop = banner.offsetTop
         let fo = $('.footer').offset().top - $(window).scrollTop();
+        let distance = $('.floor').offset().top - $('.tabs_nav_fa').height();
         if (window.pageYOffset >= banTop) {
             $('#go_nav').addClass('go_top_nav')
             $('#go_nav').css('display', 'block')
@@ -13,6 +14,11 @@ $(() => {
             $('#go_nav').addClass('go_left_nav')
         } else if (fo > $(window).height()) {
             $('#go_nav').removeClass('go_left_nav')
+        }
+        if ($(document).scrollTop() > distance) {
+            $('.tabs_nav_fa').css('position', 'fixed')
+        } else {
+            $('.tabs_nav_fa').css('position', 'relative')
         }
     })
     $('#go_nav').on('click', function () {
