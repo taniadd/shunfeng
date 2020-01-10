789a48f3e0391c208d7e060d302b30c7adde8541
fanhaoii <997824379@qq.com>
2020-01-10 16:22:50 +0800

    范豪修改了BUG

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express();
-const bodyParser=require('body-parser')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
-const dateFormat=require('dateformat')
-const template=require('art-template')
+const dateFormat = require('dateformat')
+const template = require('art-template')
 
 const path = require('path')
 const session = require('express-session')
@@ -11,18 +11,18 @@ app.use(session({
     secret: 'asdasfgsqag'
 }))
 mongoose.connect('mongodb://localhost/shunfeng', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(() => console.log('数据库连接成功'))
     .catch((err) => console.log(err, '数据库连接失败'))
 
 app.use(express.static(__dirname));
 
-template.defaults.imports.dateFormat=dateFormat;
+template.defaults.imports.dateFormat = dateFormat;
 
 //收集post请求参数
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
@@ -34,20 +34,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'art');
 // 当渲染后缀为art的模板时 所使用的模板引擎是什么
 app.engine('art', require('express-art-template'));
-const shunfeng=require('./route/WL.js')
+const shunfeng = require('./route/WL.js')
 
-const lt=require('./route/LT.js')
-app.use("/shunfeng",lt);
-
-
-const guoji=require('./route/LG.js')
-app.use("/shunfeng",guoji);
+const lt = require('./route/LT.js')
+app.use("/shunfeng", lt);
 
 
+const guoji = require('./route/LG.js')
+app.use("/shunfeng", guoji);
 
 
 
-app.use("/shunfeng",shunfeng);
+
+
+app.use("/shunfeng", shunfeng);
 
 const sf = require('./route/index')
 app.use(sf);
